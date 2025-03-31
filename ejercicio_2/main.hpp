011f9afa55cecaf4e1a9d65cd61df1d24705139c
francokir <francokir7@gmail.com>
2025-03-30 22:14:47 -0300

    ejercicio 2 empezado

---
+++ ejercicio_2/main.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class estudiante
+{
+private:
+string nombre_completo;
+int legajo;
+vector <string,float> cursos;
+
+public:
+
+};
+
+
